44061e6975d99439a2e0d9dd6a210720a56a084b
AKHIL PS <akhilpsinbox@gmail.com>
2025-02-06 13:56:41 +0100

    optimised the code

--- doc_to_pdf.py
+++ doc_to_pdf.py
@@ -1,27 +1,23 @@
 from docx2pdf import convert
 import os
 
-# Example usage
+def needs_conversion(input_file, output_file):
+    """Check if the Word file needs to be converted to PDF."""
+    return not os.path.exists(output_file) or os.path.getmtime(input_file) > os.path.getmtime(output_file)
+
+def convert_word_to_pdf(input_file, output_file):
+    """Convert a Word document to a PDF."""
+    try:
+        convert(input_file, output_file)
+        print("Conversion successful.")
+    except Exception as e:
+        print(f"Error during conversion: {e}")
+
+# File paths
 input_word_file = "CV/Akhil_PS_CV.docx"
 output_pdf_file = "CV/Akhil_PS_CV.pdf"
 
-def convrt_or_not(input_file, output_file):
-    if os.path.exists(output_file):
-        if os.path.getmtime(input_file) > os.path.getmtime(output_file):
-            return True
-        else:
-            return False
-    else:
-        return True
-
-def convert_word_to_pdf(input_file, output_file):
-    try:
-        # Convert the Word file to PDF
-        convert(input_file, output_file)
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-
-if convrt_or_not(input_word_file, output_pdf_file) == True:
-    print("Need update")
+# Perform conversion if needed
+if needs_conversion(input_word_file, output_pdf_file):
+    print("Updating PDF...")
     convert_word_to_pdf(input_word_file, output_pdf_file)
